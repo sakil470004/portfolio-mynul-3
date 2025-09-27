d8e8380e77ab94fae178c3116fc9cc3668eea55e
Mynul Islam <mynul.sakil@gmail.com>
2025-09-27 22:08:29 +0600

    clean up apps

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   // accept all images
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
